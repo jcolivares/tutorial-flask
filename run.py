7e727999b9dce3c1dcc3a8181118f2e9194c4514
Juan Carlos Olivares Rojas <juancarlosolivares@gmail.com>
2025-10-27 04:22:58 -0600

    agregando rutas para mostrar el post y publicarlo

--- run.py
+++ run.py
@@ -8,5 +8,15 @@ def index():
     return "{} posts".format(len(posts))
 
 
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return "Mostrando el post {}".format(slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return "post_form {}".format(post_id)
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
