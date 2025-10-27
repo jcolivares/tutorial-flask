0ef8bd8400eb99f2555850f2de18066296f189c2
Juan Carlos Olivares Rojas <juancarlosolivares@gmail.com>
2025-10-27 04:58:01 -0600

    cambiando metodo del signup para recibir peticiones POST y canalizar valores del formulario

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 posts = []
@@ -16,8 +16,18 @@ def show_post(slug):
 def post_form(post_id=None):
     return render_template("admin/post_form.html", post_id=post_id)
 
-@app.route("/signup/")
+@app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+
     return render_template("signup_form.html")
 
 
