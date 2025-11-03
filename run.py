4b7b792a2ff639126ad5be3d900b16a3e91a280c
Juan Carlos Olivares Rojas <juancarlosolivares@gmail.com>
2025-11-02 20:45:13 -0600

    Modificando plantilla signupform para el manejo de formularios y configuración del secreto de la APP

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
 posts = []
 
 @app.route("/")
